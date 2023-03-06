991c46bd9a24f283d7a65554aff256242c684bd2
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-03-06 18:08:23 +0500

    added model

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse, HttpResponse
+from django.contrib.auth.models import User
 
-# Create your views here.
+def get_users(request: HttpRequest) -> JsonResponse:
+    users = User.objects.all()
+    
+    res = []
+
+    for user in users:
+        res.append(
+            {
+                'id': user.id,
+                'username': user.username,
+                'password': user.password
+            }
+        )
+
+    return JsonResponse(res, safe=False)
+
+    # data = {
+    #     'id': users[0].id
+    # }
+
+    # return JsonResponse(data)
